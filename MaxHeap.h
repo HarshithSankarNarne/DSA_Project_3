a18f876456acca7d41870848ee4a0cfac929b2de
Harshith Sankar Narne <harshith.narne@gmail.com>
2025-08-03 15:49:53 -0400

    These are the algorithms

--- MaxHeap.h
+++ MaxHeap.h
@@ -12,6 +12,28 @@ class MaxHeap {
     //insert, extract, find, benchmark using chrono
     //Priority is at the top
     //Large number on top
+private:
+    int size; //getting passed in by Parham in Main.cpp
+    //We need to create an array
+    vector<Task> heap;
+
+public:
+    //Insert
+    //heapify up
+    //extractmax
+    //find
+    void insertHeap(Task* task)
+    {
+        if (arr.size() == 0)
+        {
+            heap.push_back(task);
+        }
+        for (int i = 0; i < heap.size() - 1; i++)
+        {
+            if (task.priority > heap[i].priority )
+        }
+    }
+
 
 
 };
