583b298a98dc88eeaa7f150dd6006a0c0c69dee6
Harshith Sankar Narne <harshith.narne@gmail.com>
2025-08-04 17:46:51 -0400

    Harshith: Making my Pairing Heap code more secure by moving functions into private part of the class and moving my function defintions into my cpp file. I am also using helpers for additional safety.

--- PairingHeap.cpp
+++ PairingHeap.cpp
@@ -19,10 +19,148 @@ void PairingHeap::insertHelper(int ID, int priority, std::string name)
 }
 
 
-void PairingHeap::mergeHelper(Node* one, Node* two)
+Node* PairingHeap::mergeHelper(Node* one, Node* two)
 {
+    Node* parent = nullptr;
+    if (one == nullptr)
+    {
+        return two;
+    } else if (two == nullptr)
+    {
+        return one;
+    }
 
+
+    //Compare the priority of the two nodes
+    if (one->task.priority > two->task.priority)
+    {
+        parent = one;
+        if (parent->left == nullptr)
+        {
+            parent->left = two;
+            two->sibling = nullptr;
+        } else
+        {
+            Node* temp = parent->left;
+            parent->left = two;
+            two->sibling = temp;
+        }
+
+    } else if (one->task.priority < two->task.priority)
+    {
+        parent = two;
+        if (parent->left == nullptr)
+        {
+            one->sibling = nullptr;
+            parent->left = one;
+        } else
+        {
+            Node* temp = parent->left;
+            parent->left = one;
+            one->sibling = temp;
+        }
+    } else
+    {
+        parent = one;
+        if (parent->left == nullptr)
+        {
+            parent->left = two;
+            two->sibling = nullptr;
+
+        } else
+        {
+            Node* temp = parent->left;
+            parent->left = two;
+            two->sibling = temp;
+        }
+    }
+    return parent;
 }
+
+Task PairingHeap::extractMaxHelper()
+{
+    if (root == nullptr)
+    {
+        //cout << "Empty Heap" << endl;
+        return Task(0,0,"");
+    }
+
+    Node* max = root;
+
+    std::vector<Node*> list;
+
+
+    max->sibling = nullptr;
+
+    Node* child = nullptr;
+
+    if (root->left != nullptr)
+    {
+        child = root->left;
+    }
+
+    while (child != nullptr)
+    {
+        Node* sibly = child->sibling;
+        child->sibling = nullptr;
+        list.push_back(child);
+        child = sibly;
+    }
+
+
+    std::vector<Node*> merged;
+
+    Node* last = nullptr;
+
+
+    if (list.size() == 0)
+    {
+        last = nullptr;
+    }
+    else if (list.size() == 1)
+    {
+        last = list[0];
+    }
+    else if (list.size() % 2 == 0)
+    {
+        //Left to right
+        for (unsigned long long i = 0; i < list.size(); i+= 2)
+        {
+
+            merged.push_back(merge(list[i], list[i+1]));
+        }
+        //Right to left
+        int size = merged.size() - 1;
+        last = merged.back();
+        for (int j = size - 1; j >= 0; j--)
+        {
+            last = merge(merged[j], last);
+        }
+    } else //if (list.size() % 2 != 0)
+    {
+        for (unsigned long long i = 0; i < list.size() - 1; i+= 2)
+        {
+            merged.push_back(merge(list[i], list[i+1]));
+        }
+        merged.push_back(list.back());
+        int size = merged.size() - 1;
+        last = merged.back();
+        for (int j = size - 1; j >= 0; j--)
+        {
+            last = merge(merged[j], last);
+        }
+    }
+
+
+
+
+
+    Task maxExtract = max->task;
+    root = last;
+    delete max;
+    return maxExtract;
+}
+
 
 
 
