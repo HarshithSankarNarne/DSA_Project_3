7b95f2e09bd16c48476d3ae34ba1c424063bbd6a
Harshith Sankar Narne <harshith.narne@gmail.com>
2025-08-04 13:05:03 -0400

    Harshith: Fixed the bugs in Task struct

--- Task.h
+++ Task.h
@@ -2,10 +2,16 @@
 #include <string>
 #include <utility>
 
+
+
+
+
 struct Task {
     int ID;
     int priority;
     std::string name;
+
+    Task() : ID(0), priority(0), name("") {}
 
     Task(int ID, int priority, std::string name) : ID(ID), priority(priority), name(std::move(name)) {}
 };
