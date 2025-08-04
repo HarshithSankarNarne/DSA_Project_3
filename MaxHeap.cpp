0bceae923e39f36584c9e66953fa9dfcdca29006
DavidSebastian05 <166626027+DavidSebastian05@users.noreply.github.com>
2025-08-04 10:44:04 -0400

    Update MaxHeap.cpp

--- MaxHeap.cpp
+++ MaxHeap.cpp
@@ -1,5 +1,77 @@
-//
-// Created by Harshith Sankar Narne on 8/3/25.
-//
-
 #include "MaxHeap.h"
+#include <vector>
+using namespace std;
+
+void MaxHeap::insert(Task task) {
+    if(heap.size()==0) {
+        heap.push_back(task);
+    }
+    else {
+        heap.push_back(task);
+        heapifyUp(heap.size()-1);
+    }
+};
+
+void MaxHeap::heapifyUp(vector<Task>::size_type taskIndex) {
+    vector<Task>::size_type<> parentIndex = (taskIndex-1)/2;
+    if(taskIndex ==0 || heap[parentIndex].priority >= heap[taskIndex].priority) {
+        return;
+    }
+    else {
+        swap(heap[taskIndex], heap[parentIndex]);
+        heapifyUp(parentIndex);
+    };
+
+};
+
+//Understanding heapifyDown: https://www.geeksforgeeks.org/dsa/introduction-to-max-heap-data-structure/
+void MaxHeap::heapifyDown(vector<Task>::size_type num) {
+    vector<Task>::size_type left = 2 * num + 1;
+    vector<Task>::size_type right= 2 * num + 2;
+    vector<Task>::size_type biggest = num;
+    if(left < heap.size() && heap[left].priority > heap[biggest].priority) {
+        biggest = left;
+    }
+    if(right < heap.size() && heap[right].priority > heap[biggest].priority) {
+        biggest = right;
+    }
+    if (biggest != num) {
+        swap(heap[num], heap[biggest]);
+        heapifyDown(biggest);
+    }
+
+};
+
+Task MaxHeap::extractMax() {
+    if(heap.size()==0) {
+        Task emptyTask{-1, -1, "null"};
+        return emptyTask;
+        }
+    vector<Task>::size_type zero = 0;
+    Task returnVal = heap[zero];
+    heap[zero]=heap.back();
+    heap.pop_back();
+    if(heap.size()!=0) {
+        heapifyDown(0);
+    }
+
+
+    return returnVal;
+};
+
+Task MaxHeap::peek() {
+    if(heap.size()==0) {
+        Task emptyTask{-1, -1, "null"};
+        return emptyTask;
+    }
+    vector<Task>::size_type zero = 0;
+    return heap[zero];
+};
+
+void MaxHeap::clear() {
+    while(heap.size() != 0) {
+        extractMax();
+    }
+};
+
+
