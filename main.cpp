b9bc2e27dd71ec2e974ba08a67d339e6be2867d1
pmarashi5 <p.marashi@ufl.edu>
2025-08-04 14:47:06 -0400

    formatting

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main()
                     auto maxPostRemoveTime = outPair.first;
                     auto pairingPostRemoveTime = outPair.second;
                     std::string timerValue = benchmark.getTimer() ? "Timer ON" : "Timer OFF";
-                    std::cout << "Summary:\n" + timerValue + "\nMaxHeap:\t\tinsertion " + std::to_string(maxPostInsertTime.count()) + " ns, deletion " + std::to_string(maxPostRemoveTime.count()) + " ns, sum " + std::to_string(maxPostInsertTime.count() + maxPostRemoveTime.count()) + " ns\nPairing Heap:\tinsertion " + std::to_string(pairingPostInsertTime.count()) + " ns, deletion " + std::to_string(pairingPostRemoveTime.count()) + " ns, sum " + std::to_string(pairingPostInsertTime.count() + pairingPostRemoveTime.count()) + " ns\n" << std::endl;
+                    std::cout << "Summary:\n" + timerValue + "\nMaxHeap:        insertion " + std::to_string(maxPostInsertTime.count()) + " ns, deletion " + std::to_string(maxPostRemoveTime.count()) + " ns, sum " + std::to_string(maxPostInsertTime.count() + maxPostRemoveTime.count()) + " ns\nPairing Heap:   insertion " + std::to_string(pairingPostInsertTime.count()) + " ns, deletion " + std::to_string(pairingPostRemoveTime.count()) + " ns, sum " + std::to_string(pairingPostInsertTime.count() + pairingPostRemoveTime.count()) + " ns\n" << std::endl;
                     seenMenu = false;
                 }
             }
